fb13cbb32d4ab776e4be324739a8f1085f7f7195
Frostetsky <nesterenko-nikita@bk.ru>
2020-11-28 23:48:12 +0300

    Spring Scope Bean

--- src/Spring_IoC/IoC/Main.java
+++ src/Spring_IoC/IoC/Main.java
@@ -3,6 +3,7 @@ package Spring_IoC.IoC;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("ApplicationContext1.xml");

e1e37446b12851df46f43e2922a44a3e843b0963
Frostetskiy <nesterenko-nikita@bk.ru>
2021-05-22 19:26:46 +0300

    Процедура авторизации

--- Spring_Security/src/main/java/spring_security/configuration/MySecurityConfig.java
+++ Spring_Security/src/main/java/spring_security/configuration/MySecurityConfig.java
@@ -2,6 +2,7 @@ package spring_security.configuration;
 
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -24,5 +25,16 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(builder.username("Andrey")
                                  .password("321")
                                  .roles("Manager", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("Employee", "HR", "Manager")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info").hasRole("Manager")
+                .and()
+                .formLogin()
+                .permitAll();
     }
 }
--- Spring_Security/src/main/webapp/WEB-INF/view/view-for-all-employees.jsp
+++ Spring_Security/src/main/webapp/WEB-INF/view/view-for-all-employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE HTML>
 
 <html>
@@ -6,11 +7,16 @@
 
 <h2> Information for All Employees </h2>
 <br>
+<security:authorize access="hasRole('HR')">
 <input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
 Only for HR Staff
+</security:authorize>
 <br>
+<br>
+<security:authorize access="hasRole('Manager')">
 <input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
 Only for Managers
+</security:authorize>
 <br>
 </body>
 

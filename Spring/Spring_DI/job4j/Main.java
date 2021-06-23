ad145a0476415db99c3b7d4b120b843ac3e34028
Frostetskiy <nesterenko-nikita@bk.ru>
2021-06-23 19:13:47 +0300

    Spring DI

--- Spring/Spring_DI/job4j/Main.java
+++ Spring/Spring_DI/job4j/Main.java
@@ -1,15 +1,19 @@
 package Spring_DI.job4j;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Context context = new Context();
-        context.reg(Store.class);
-        context.reg(StartUI.class);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(StartUI.class);
+        applicationContext.register(Store.class);
+        applicationContext.refresh();
 
-        StartUI ui = context.get(StartUI.class);
 
+        StartUI ui = applicationContext.getBean(StartUI.class);
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
+
     }
 }
